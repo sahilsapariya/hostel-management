7275d666b445225852ed9b9c660545d7e0abef61
sahilsapariya <sahilsapariya03@gmail.com>
2023-12-10 23:15:02 +0530

    created all the views with testing: completed APIs for guards, cooks, cleaners

--- server/profiles/urls.py
+++ server/profiles/urls.py
@@ -3,7 +3,20 @@ from .views import *
 
 urlpatterns = [
     path('test', HomeView.as_view(), name="home"),
+    
     path('students/', StudentsView.as_view(), name="students-view"),
-    path('student/', StudentsView.as_view(), name="student-view"),
+    path('student/', StudentView.as_view(), name="student-view"),
     path('student/<int:pk>/', StudentView.as_view(), name="student-detail"),
+
+    path('cleaners/', CleanersView.as_view(), name="cleaners-view"),
+    path('cleaner/', CleanerView.as_view(), name="cleaner-view"),
+    path('cleaner/<int:pk>/', CleanerView.as_view(), name="cleaner-detail"),
+
+    path('cooks/', CooksView.as_view(), name="cooks-view"),
+    path('cook/', CookView.as_view(), name="cook-view"),
+    path('cook/<int:pk>/', CookView.as_view(), name="cook-detail"),
+
+    path('guards/', GuardsView.as_view(), name="guards-view"),
+    path('guard/', GuardView.as_view(), name="guard-view"),
+    path('guard/<int:pk>/', GuardView.as_view(), name="guard-detail"),
 ]
--- server/profiles/views.py
+++ server/profiles/views.py
@@ -65,3 +65,145 @@ class StudentView(APIView):
         student = Students.objects.get(pk=pk)
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+class CleanersView(APIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        cleaners = Cleaners.objects.all()
+        serializer = CleanersSerializer(cleaners, many=True)
+        return Response(data=serializer.data)
+
+
+class CleanerView(APIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        try:
+            cleaner = Cleaners.objects.get(pk=pk)
+            serializer = CleanersSerializer(cleaner, many=False)
+            return Response(serializer.data)
+        except Cleaners.DoesNotExist:
+            return Response({"error": "Cleaner not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        serializer = CleanersSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        
+        cleaner = Cleaners.objects.get(pk=pk)
+        
+        serializer = CleanersSerializer(cleaner, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        cleaner = Cleaners.objects.get(pk=pk)
+        cleaner.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CooksView(APIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        cooks = Cooks.objects.all()
+        serializer = CooksSerializer(cooks, many=True)
+        return Response(data=serializer.data)
+
+
+class CookView(APIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        try:
+            cook = Cooks.objects.get(pk=pk)
+            serializer = CooksSerializer(cook, many=False)
+            return Response(serializer.data)
+        except Cooks.DoesNotExist:
+            return Response({"error": "Cook not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        serializer = CooksSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        
+        cook = Cooks.objects.get(pk=pk)
+        
+        serializer = CooksSerializer(cook, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        cook = Cooks.objects.get(pk=pk)
+        cook.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class GuardsView(APIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        guards = Guards.objects.all()
+        serializer = GuardsSerializer(guards, many=True)
+        return Response(data=serializer.data)
+
+
+class GuardView(APIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        try:
+            guard = Guards.objects.get(pk=pk)
+            serializer = GuardsSerializer(guard, many=False)
+            return Response(serializer.data)
+        except Guards.DoesNotExist:
+            return Response({"error": "Guard not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        serializer = GuardsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        
+        guard = Guards.objects.get(pk=pk)
+        
+        serializer = GuardsSerializer(guard, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        guard = Guards.objects.get(pk=pk)
+        guard.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
