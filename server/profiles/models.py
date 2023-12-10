c536107e5e9f55ce3ff0ef3cbd1e1bfa486a760b
sahilsapariya <sahilsapariya03@gmail.com>
2023-12-10 22:27:06 +0530

    created cook, guard and clearner models

--- server/profiles/models.py
+++ server/profiles/models.py
@@ -20,3 +20,55 @@ class Students(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Clearns(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    aadhar_number = models.CharField(max_length=100)
+    cast = models.CharField(max_length=20)
+    emergency_contact = models.CharField(max_length=20)
+    salary = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Cooks(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    aadhar_number = models.CharField(max_length=100)
+    cast = models.CharField(max_length=20)
+    emergency_contact = models.CharField(max_length=20)
+    salary = models.FloatField()
+    experience = models.TextField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+
+class Guards(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    aadhar_number = models.CharField(max_length=100)
+    cast = models.CharField(max_length=20)
+    emergency_contact = models.CharField(max_length=20)
+    salary = models.FloatField()
+    experience = models.TextField(max_length=20)
+    shift_start = models.TimeField()
+    shift_end = models.TimeField()
+
+    def __str__(self) -> str:
+        return self.name
