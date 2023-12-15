ce2ff61fcc3e7ed358e056e87d3826cd7c1b24a9
sahilsapariya <sahilsapariya03@gmail.com>
2023-12-15 19:58:31 +0530

    completed profile side

--- client/src/pages/profiles/operation/add/AddCleaner.jsx
+++ client/src/pages/profiles/operation/add/AddCleaner.jsx
@@ -1,8 +1,200 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const AddCleaner = () => {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const [address, setAddress] = useState();
+  const [phone, setPhone] = useState();
+  const [email, setEmail] = useState();
+  const [emergencyContact, setEmergencyContact] = useState();
+  const [gender, setGender] = useState();
+  const [aadharNumber, setAadharNumber] = useState();
+  const [cast, setCast] = useState();
+  const [salary, setSalary] = useState();
+
+
+  const data = {
+    name: name,
+    age: age,
+    address: address,
+    phone: phone,
+    email: email,
+    emergency_contact: emergencyContact,
+    gender: gender,
+    aadhar_number: aadharNumber,
+    cast: cast,
+    salary: salary
+  };
+
+
+  const addData = async (url, data) => {
+    try {
+      console.log(url);
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      navigate("/profiles");
+    }
+  };
+
+
   return (
-    <div>AddCleaner</div>
+    <form
+      onSubmit={() => addData("http://127.0.0.1:8000/profiles/cleaner/", data)}
+    >
+      <table className="information_table">
+        <tr>
+          <td>
+            <th>Name</th>
+          </td>
+          <td>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter full name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <th>Age</th>
+          </td>
+          <td>
+            <input
+              type="text"
+              name="age"
+              placeholder="Enter age"
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <th>Address</th>
+          </td>
+          <td>
+            <input
+              type="text"
+              name="address"
+              placeholder="Enter address"
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <th>Phone number</th>
+          </td>
+          <td>
+            <input
+              type="text"
+              name="phone_number"
+              placeholder="Enter phone number"
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <th>Email</th>
+          </td>
+          <td>
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <th>Gender</th>
+          </td>
+          <td>
+            <select
+              id="gender"
+              name="gender"
+              onChange={(e) => setGender(e.target.value)}
+            >
+              <option value="">Select</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <th>Aadhar number</th>
+          </td>
+          <td>
+            <input
+              type="text"
+              name="aadhar number"
+              placeholder="Enter Aadhar number"
+              onChange={(e) => setAadharNumber(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <th>Cast</th>
+          </td>
+          <td>
+            <input
+              type="text"
+              name="cast"
+              placeholder="Enter cast"
+              onChange={(e) => setCast(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <th>Emergency contact number</th>
+          </td>
+          <td>
+            <input
+              type="text"
+              name="emergency contact number"
+              placeholder="Enter emergency contact number"
+              onChange={(e) => setEmergencyContact(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <th>Salary</th>
+          </td>
+          <td>
+            <input
+              type="number"
+              name="salary"
+              placeholder="Enter salary"
+              onChange={(e) => setSalary(e.target.value)}
+            />
+          </td>
+        </tr>
+      </table>
+      <button type="submit">Add Cleaner</button>
+    </form>
   )
 }
 
