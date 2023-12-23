a914d15fabd4997c6278d2316325730973d61a0e
sahilsapariya <sahilsapariya03@gmail.com>
2023-12-23 15:47:00 +0530

    created feature of filter in room listing in client side

--- client/src/pages/rooms/Rooms.jsx
+++ client/src/pages/rooms/Rooms.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import { useNavigate } from "react-router-dom";
 
 const Rooms = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  // State variables for filtering
+  const [minCapacity, setMinCapacity] = useState(0);
+  const [maxRent, setMaxRent] = useState(Number.MAX_SAFE_INTEGER);
+  const [isFilterActive, setIsFilterActive] = useState(false);
+
   const {
     data: rooms,
     loading,
     error,
   } = useFetch("http://localhost:8000/rooms/");
 
-  if (error) return <div>Some thing went wrong : {error}</div>
+  if (error) return <div>Some thing went wrong : {error}</div>;
+
+
+  // Filtering logic
+  const filteredRooms = rooms?.filter(
+    (room) =>
+      (!isFilterActive || (room.capacity >= minCapacity && room.rent <= maxRent))
+  );
+
+  const handleFilterApply = () => {
+    setIsFilterActive(true);
+  };
+
+  const handleFilterReset = () => {
+    setIsFilterActive(false);
+    setMinCapacity(0);
+    setMaxRent(Number.MAX_SAFE_INTEGER);
+  };
 
 
   return (
@@ -20,16 +43,48 @@ const Rooms = () => {
       </div>
 
       <div className="seperation_section">
-        <button type="button" onClick={() => navigate('/rooms/add')}>Add Room</button>
+        <button type="button" onClick={() => navigate("/rooms/add")}>
+          Add Room
+        </button>
         <hr />
       </div>
 
-      <div className="items_list">
+      <div className="filter_section">
+        <label>Minimum Capacity:</label>
+        <input
+          type="number"
+          value={minCapacity}
+          onChange={(e) => setMinCapacity(parseInt(e.target.value, 10))}
+        />
+
+        <label>Maximum Rent:</label>
+        <input
+          type="number"
+          value={maxRent}
+          onChange={(e) => setMaxRent(parseInt(e.target.value, 10))}
+        />
+
+        <button onClick={handleFilterApply}>Apply Filters</button>
+        <button onClick={handleFilterReset}>Reset Filters</button>
+      </div>
+
+      {/* <div className="items_list">
         <ul>
           {!loading &&
             rooms.map((room, index) => {
               return <li key={index} onClick={() => navigate(`/rooms/${room.id}`)}>Room Number : {room.room_number}</li>;
             })}
+        </ul>
+      </div> */}
+
+      <div className="items_list">
+        <ul>
+          {!loading &&
+            filteredRooms.map((room) => (
+              <li key={room.id} onClick={() => navigate(`/rooms/${room.id}`)}>
+                Room Number: {room.room_number}
+              </li>
+            ))}
         </ul>
       </div>
     </div>
