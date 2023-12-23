d715c3dd1a4da16fba2080e8cbea670648f9a9c3
sahilsapariya <sahilsapariya03@gmail.com>
2023-12-23 23:25:19 +0530

    update build_files.sh file

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-python3.11.5 -m pip install -r requirements.txt
-python3.11.5 manage.py collectstatic --noinput --clear
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
 echo "BUILD END"
 
