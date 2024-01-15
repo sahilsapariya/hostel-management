dfbfbeadb86341fb8e774d739a0b5664c547f80d
sahilsapariya <sahilsapariya03@gmail.com>
2024-01-15 23:43:30 +0530

    changed backend url

--- client/src/config.js
+++ client/src/config.js
@@ -2,6 +2,6 @@
 //   ? 'https://sahilsapariya.pythonanywhere.com'
 //   : 'http://127.0.0.1:8000';
 
-// const baseurl = "https://sahilsapariya.pythonanywhere.com"
-const baseurl = "http://127.0.0.1:8000";
+const baseurl = "https://sahilsapariya.pythonanywhere.com"
+// const baseurl = "http://127.0.0.1:8000";
 export default baseurl;
